ff070fd0466158904aa97e810b3e3a8706d578c1
Varadekushagra <kushagra@robustest.com>
2019-12-06 11:01:31 +0530

    Grouping of routes

--- main.go
+++ main.go
@@ -11,13 +11,21 @@ func main(){
 	router := gin.Default()
 	fmt.Println("Starting JSON server at 3000")
 
-	// Calling different methods to hanlde user
-	router.GET("/users" , Methods.GetAllUsers)
-	router.GET("/user/:name" , Methods.GetUserDetail) // Passing params
-	router.POST("/user/new" , Methods.CreateNewUser) // Passing params 
-	router.PUT("/user/:name" , Methods.UpdateUserDetail) // Passing params
-	router.DELETE("/users" , Methods.DeleteAllUsers)
-	router.DELETE("/user/:name" , Methods.DeleteUserDetail) // Passing params
+	// Handling static routes
+	router.LoadHTMLGlob("../resource/index.html")
 
+	// Calling different methods to hanlde user grouping this routes as version v1
+	v1 := router.Group("/v1")
+	
+	{
+		v1.GET("/users" , Methods.GetAllUsers)
+		v1.GET("/user/:name" , Methods.GetUserDetail) // Passing params
+		v1.POST("/user/new" , Methods.CreateNewUser) // Passing params 
+		v1.PUT("/user/:name" , Methods.UpdateUserDetail) // Passing params
+		v1.DELETE("/users" , Methods.DeleteAllUsers)
+		v1.DELETE("/user/:name" , Methods.DeleteUserDetail) // Passing params
+	}
+
+	// Running the server on port 3000
 	router.Run(":3000")
 }
